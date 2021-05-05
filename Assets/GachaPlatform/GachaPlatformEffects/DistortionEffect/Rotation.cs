3d1ea02227f890f2b13ced16476ba0962451e84c
Aaron <aaron.martinez.7e3@itb.cat>
2021-05-06 00:01:35 +0200

    BEAT-6088 Added MusicBox sprite, fixed particle bugs

---
+++ Assets/GachaPlatform/GachaPlatformEffects/DistortionEffect/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+
+public class Rotation : MonoBehaviour {
+
+    public Vector3 RotateAxis = new Vector3(1, 5, 10);
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void Update () {
+        transform.Rotate(RotateAxis * (speed * Time.deltaTime));
+	}
+}
