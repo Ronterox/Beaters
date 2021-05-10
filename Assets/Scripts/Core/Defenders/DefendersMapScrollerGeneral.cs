4e88479e3f9980370af1dad9cc36267c40e4dabc
Ricardo <ricarditomontserrat@gmail.com>
2021-05-10 21:25:23 +0200

    Merge Fixes

--- Assets/Scripts/Core/Defenders/DefendersMapScrollerGeneral.cs
+++ Assets/Scripts/Core/Defenders/DefendersMapScrollerGeneral.cs
@@ -1,23 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using Managers;
 using UnityEngine;
 using Plugins.Audio;
+using Utilities;
 
 namespace Core.Defenders
 {
     public class DefendersMapScrollerGeneral : MonoBehaviour
     {
-        public DefendersMapScroller defedenderMapScrollerUp, defedenderMapScrollerRight, defedenderMapScrollerLeft, defedenderMapScrollerDown;
+        public DefendersMapScroller scrollerUp, scrollerRight, scrollerLeft, scrollerDown;
+        public Difficulty difficulty;
         public SoundMap soundMap;
 
-        private void Start() {
-            float bps = soundMap.bpm / 60 * (float)soundMap.difficulty;
-            defedenderMapScrollerUp.StartMap(bps);
-            defedenderMapScrollerDown.StartMap(bps);
-            defedenderMapScrollerRight.StartMap(bps);
-            defedenderMapScrollerLeft.StartMap(bps);
-            SoundManager.Instance.PlayBackgroundMusicInstantly(soundMap.mapSong);
+        private void Start()
+        {
+            float bps = soundMap.bpm / 60 * (float)difficulty;
+            scrollerUp.StartMap(bps);
+            scrollerDown.StartMap(bps);
+            scrollerRight.StartMap(bps);
+            scrollerLeft.StartMap(bps);
+
+            SoundManager.Instance.PlayBackgroundMusicNoFade(soundMap.audioClip);
         }
     }
 }
-
