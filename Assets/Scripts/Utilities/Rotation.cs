da5f20907d159dbecb3717fb6d1dcb031a6a0e47
Ricardo <ricarditomontserrat@gmail.com>
2021-05-11 21:05:14 +0200

    Merge Fixes

---
+++ Assets/Scripts/Utilities/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    [ExecuteInEditMode]
+    public class Rotation : MonoBehaviour
+    {
+        public Vector3 rotateAxis = new Vector3(1, 5, 10);
+        public float speed;
+
+
+        private void Update() => transform.Rotate(rotateAxis * (speed * Time.deltaTime));
+    }
+}
