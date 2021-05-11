98d99904f939d46d971244574c83f226939d996b
Ricardo <ricarditomontserrat@gmail.com>
2021-05-11 21:13:53 +0200

    Tag Manager Fix

--- Assets/Scripts/Utilities/Rotation.cs
+++ Assets/Scripts/Utilities/Rotation.cs
@@ -8,7 +8,6 @@ namespace Utilities
         public Vector3 rotateAxis = new Vector3(1, 5, 10);
         public float speed;
 
-
         private void Update() => transform.Rotate(rotateAxis * (speed * Time.deltaTime));
     }
 }
