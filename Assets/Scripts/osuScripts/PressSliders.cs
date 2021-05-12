7717ef76cd71a7a8c54f7a1592b6ab07e3c70c68
Aaron <aaron.martinez.7e3@itb.cat>
2021-05-12 16:24:53 +0200

    BEAT-6625 Slider script completed

--- Assets/Scripts/osuScripts/PressSliders.cs
+++ Assets/Scripts/osuScripts/PressSliders.cs
@@ -2,45 +2,8 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class PressSliders : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class PressSliders : MonoBehaviour
 {
-    private bool pointerDown;
-    private float pointerDownTimer;
-
-    public float requiredHoldTime;
-
-    public UnityEvent onLongClick;
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        pointerDown = true;
-        Debug.Log("Manteniendo pulsado");
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        Reset();
-        Debug.Log("Soltado");
-    }
-
-    void Update()
-    {
-        if (pointerDown)
-        {
-            pointerDownTimer += Time.deltaTime;
-            if(pointerDownTimer >= requiredHoldTime)
-            {
-                if(onLongClick != null)
-                {
-                    onLongClick.Invoke();
-                }
-                Reset();
-            }
-        }
-    }
-
-    private void Reset()
-    {
-        pointerDown = false;
-        pointerDownTimer = 0;
-    }
+    
+    
 }
--- Assets/Scripts/osuScripts/SliderControl.cs
+++ Assets/Scripts/osuScripts/SliderControl.cs
@@ -3,15 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using Plugins.Tools;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class SliderControl : MonoBehaviour
 {
+    private bool pointerDown;
+    private float pointerDownTimer;
+
+    public UnityEvent onLongClick;
+
     public List<Transform> waypoints;
     public float durationMove;
 
     void Start()
     {        
         StartCoroutine(Movement());
+    }
+
+    public void OnMouseDown()
+    {
+        pointerDown = true;
+        Debug.Log("Manteniendo pulsado");
+    }
+
+    public void OnMouseUp()
+    {
+        Reset();
+        Debug.Log("Soltado");
+    }
+
+    void Update()
+    {
+        if (pointerDown)
+        {
+            pointerDownTimer += Time.deltaTime;
+            if(pointerDownTimer >= durationMove)
+            {
+                if(onLongClick != null)
+                {
+                    onLongClick.Invoke();
+                }
+                Reset();
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        pointerDown = false;
+        pointerDownTimer = 0;
     }
 
      private IEnumerator Movement(){
