5d3b7eb65e4ed1be1f01b2248dcf1cb1f5601032
Ricardo <ricarditomontserrat@gmail.com>
2021-05-18 00:26:36 +0200

    BEAT-7181 Character description and skills descriptions

--- Assets/Scripts/Utilities/CharacterScriptableSelector.cs
+++ Assets/Scripts/Utilities/CharacterScriptableSelector.cs
@@ -2,13 +2,22 @@ using System.Collections.Generic;
 using Managers;
 using ScriptableObjects;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Utilities
 {
     public class CharacterScriptableSelector : ScriptableSelector<ScriptableCharacter>
     {
         public GUIManager guiManager;
+        [Space]
         public TMP_Text characterName;
+        public TMP_Text characterDescription;
+        [Space]
+        public Image activeSkillImage;
+        public Image passiveSkillImage;
+        
+        public TMP_Text activeSkillText, passiveSkillText;
 
         protected override List<ushort> GetObjectIds() => DataManager.GetCharactersIds();
 
@@ -17,7 +26,15 @@ namespace Utilities
             ScriptableCharacter character = m_PlayerObjects[m_Index];
 
             objectImage.sprite = character.sprites[0];
+            
             characterName.text = character.characterName;
+            characterDescription.text = character.description;
+
+            activeSkillText.text = character.activeSkill.effectDescription;
+            passiveSkillText.text = character.passiveSkill.effectDescription;
+
+            activeSkillImage.sprite = character.activeSkill.skillImage;
+            passiveSkillImage.sprite = character.passiveSkill.skillImage;
 
             GameManager.PutCharacter(character);
             guiManager.SetCharacterGUI(character);
