228d93f98238f830c7e8c95c11992d6b3ec801be
Ricardo <ricarditomontserrat@gmail.com>
2021-05-20 22:41:06 +0200

    BEAT-7233 Moved fonts, reduce quality of textures, added time to record screen

--- Assets/Scripts/UI/SongRecordScreen.cs
+++ Assets/Scripts/UI/SongRecordScreen.cs
@@ -9,10 +9,12 @@ namespace UI
 {
     public class SongRecordScreen : MonoBehaviour
     {
-        public Image songRecordImage;
+        public Image songRecordImage, characterImage;
+        
         [Space]
-        public Image characterImage;
-        public TMP_Text scoreText, gradeText, comboText, accuracyText;
+        public TMP_Text scoreText;
+        public TMP_Text gradeText, comboText, accuracyText, songTimeText;
+        
         [Space]
         public Button playButton;
         private Song m_Song;
@@ -23,13 +25,14 @@ namespace UI
             LevelLoadManager.LoadArrowGameplayScene();
         });
         
-        public void ShowRecordScreen(Sprite sprite, int score, string grade, int combo, float accuracy)
+        public void ShowRecordScreen(Sprite sprite, int score, string grade, int combo, float accuracy, float time)
         {
             characterImage.sprite = sprite;
             scoreText.text = $"Score: {score}";
             comboText.text = $"Highest Combo: {combo}";
             accuracyText.text = $"Accuracy: {accuracy}%";
             gradeText.text = $"Grade: {grade}";
+            songTimeText.text = $"Length: {Mathf.Floor(time * 0.016665f) % 60:00}:{time % 60:00}";
         }
 
         public void ShowRecordScreen(Song song)
@@ -40,7 +43,10 @@ namespace UI
 
             SerializableSong serializableSong = DataManager.GetSong(song.ID);
             
-            ShowRecordScreen(GameManager.GetCharacter().sprites[0], serializableSong.highestScore, "SSS", serializableSong.highestCombo, serializableSong.accuracy);
+            ShowRecordScreen(GameManager.GetCharacter().sprites[0], serializableSong.highestScore, 
+                             "SSS", 
+                             serializableSong.highestCombo, 
+                             serializableSong.accuracy, song.soundMap.audioClip.length);
             
             m_Song = song;
 
