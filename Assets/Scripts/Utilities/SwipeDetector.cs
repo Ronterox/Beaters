4462212efa5cfeaf9f6e165dc19ac81837d59a3d
Ricardo <ricarditomontserrat@gmail.com>
2021-05-28 14:20:27 +0200

    BEAT-7683 Added Swipe Detection

---
+++ Assets/Scripts/Utilities/SwipeDetector.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace Utilities
+{
+    public enum Direction : byte { None, North, NorthEast, NorthWest, South, SouthEast, SouthWest, East, West }
+
+    public class SwipeDetector : MonoBehaviour
+    {
+        public float swipeThreshold = 50f, timeThreshold = 0.3f;
+
+        private Vector2 m_FingerDown, m_FingerUp;
+        private float m_FingerDownTime, m_FingerUpTime;
+
+        public delegate void SwipeEvent(Direction direction);
+
+        public event SwipeEvent onSwipe;
+
+        private void Start()
+        {
+            onSwipe += x => print(x.ToString());
+        }
+
+        private void Update()
+        {
+#if !UNITY_ANDROID && !UNITY_IPHONE
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_FingerDown = m_FingerUp = Input.mousePosition;
+                m_FingerDownTime = Time.time;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                m_FingerDown = Input.mousePosition;
+                m_FingerUpTime = Time.time;
+                CheckSwipe();
+            }
+#else
+            foreach (Touch touch in Input.touches)
+            {
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        m_FingerDown = touch.position;
+                        m_FingerUp = touch.position;
+                        m_FingerDownTime = Time.time;
+                        break;
+                    case TouchPhase.Ended:
+                        m_FingerDown = touch.position;
+                        m_FingerUpTime = Time.time;
+                        CheckSwipe();
+                        break;
+                }
+            }
+#endif
+        }
+
+        private void CheckSwipe()
+        {
+            float duration = m_FingerUpTime - m_FingerDownTime;
+            if (duration > timeThreshold) return;
+
+            Direction horizontalDirection = Direction.None, verticalDirection = Direction.None;
+
+            float deltaX = m_FingerDown.x - m_FingerUp.x;
+            if (Mathf.Abs(deltaX) > swipeThreshold)
+            {
+                if (deltaX > 0) horizontalDirection = Direction.East;
+                else if (deltaX < 0) horizontalDirection = Direction.West;
+            }
+
+            float deltaY = m_FingerDown.y - m_FingerUp.y;
+            if (Mathf.Abs(deltaY) > swipeThreshold)
+            {
+                if (deltaY > 0) verticalDirection = Direction.North;
+                else if (deltaY < 0) verticalDirection = Direction.South;
+            }
+
+            m_FingerUp = m_FingerDown;
+
+            Direction swipeDirection = MergeDirections(horizontalDirection, verticalDirection);
+            if(swipeDirection != Direction.None) onSwipe?.Invoke(swipeDirection);
+        }
+
+        /// <summary>
+        /// Merges 2 directions, Example: north and east, becomes NorthEast
+        /// </summary>
+        /// <param name="horizontal"></param>
+        /// <param name="vertical"></param>
+        /// <returns></returns>
+        private static Direction MergeDirections(Direction horizontal, Direction vertical) =>
+            vertical switch
+            {
+                Direction.North => horizontal switch
+                {
+                    Direction.East => Direction.NorthEast, 
+                    Direction.West => Direction.NorthWest, 
+                    _ => Direction.North
+                },
+                Direction.South => horizontal switch
+                {
+                    Direction.East => Direction.SouthEast, 
+                    Direction.West => Direction.SouthWest,
+                    _ => Direction.South
+                },
+                _ => horizontal
+            };
+    }
+}
